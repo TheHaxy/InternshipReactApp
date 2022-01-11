031e80481a96a7dde91bd86a32c0b91324c11f35
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-11 13:05:11 +0300

    add content

--- src/components/HeaderComponent/Header.jsx
+++ src/components/HeaderComponent/Header.jsx
@@ -4,7 +4,9 @@ import headerClasses from './Header.module.css'
 const Header = () => {
     return (
         <div className={headerClasses.header}>
-
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci animi, dolorum exercitationem,
+            illo illum impedit inventore laudantium molestiae natus quibusdam sapiente suscipit. Accusantium asperiores
+            facere illum maxime obcaecati quis.
         </div>
     );
 };
