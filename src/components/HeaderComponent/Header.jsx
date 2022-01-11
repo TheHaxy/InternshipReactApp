3535dba9f3bee37ef702cecfa7ab76d133726ccf
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-11 14:47:06 +0300

    fix headerComponent

--- src/components/HeaderComponent/Header.jsx
+++ src/components/HeaderComponent/Header.jsx
@@ -4,8 +4,8 @@ import headerClasses from './Header.module.css'
 const Header = () => {
     return (
         <div className={headerClasses.header}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci animi, dolorum exercitationem,
-            illo illum impedit inventore laudantium molestiae natus quibusdam sapiente suscipit. Accusantium asperiores
+            Lorem ipsum dolor sit, consectetur adipisicing elit. Accusamus adipisci animi, dolorum exercitationem,
+            illo illum impedit inventore laudantium molestiae
         </div>
     );
 };
