f3cbba00c262716c123baa2cb471b012488e3c0b
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-11 13:03:20 +0300

    create new component

---
+++ src/components/HeaderComponent/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import headerClasses from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={headerClasses.header}>
+
+        </div>
+    );
+};
+
+export default Header;
