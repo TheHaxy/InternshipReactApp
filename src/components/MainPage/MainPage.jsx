a981f844b3be35b6b95e5f419888524f2230160d
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-13 12:00:29 +0300

    fix main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import mainClasses from './MainPage.module.css'
+import React from "react";
+import mainClasses from "./MainPage.module.css";
 import Article from "../Article/Article";
 import ArticleList from "../ArticleList/ArticleList";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const MainPage = () => {
-    return (
-        <>
-            <Header/>
-            <main className={mainClasses.main}>
-                <div className={mainClasses.wrap}>
-                    <Article location='main_page'/>
-                    <ArticleList/>
-                </div>
-            </main>
-            <Footer/>
-        </>
-    )
-        ;
+  return (
+    <>
+      <Header />
+      <main className={mainClasses.main}>
+        <Article location="main_page" />
+        <ArticleList />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default MainPage;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React from "react";
 import navClasses from "./Nav.module.css";
-import {ReactComponent as Logo} from "../../assets/Logo.svg";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
 import Button from "../UI/Button/Button";
 import { useNavigate } from "react-router-dom";
 
-const Nav = ({location}) => {
-    const navigate = useNavigate();
-    return (
-        <nav className={navClasses[`${location}__nav`]}>
-            <div className={navClasses.wrap}>
-                <Logo className={navClasses[`${location}__logo`]} src={Logo} alt='Logo'/>
-                <div className={navClasses[`header__buttons`]}>
-                    <Button variant={`outlined__${location}`} name='Log in' onClick={() => navigate('/login')}/>
-                    <Button variant={`contained__${location}`} name='Sign in'/>
-                </div>
-            </div>
-        </nav>
-    );
+const Nav = ({ location }) => {
+  const navigate = useNavigate();
+  return (
+    <nav className={navClasses[`${location}__nav`]}>
+      <Logo
+        className={navClasses[`${location}__logo`]}
+        src={Logo}
+        alt="Logo"
+        onClick={() => navigate("/main-page")}
+      />
+      <div className={navClasses[`header__buttons`]}>
+        <Button
+          variant={`outlined__${location}`}
+          name="Log in"
+          onClick={() => navigate("/login")}
+        />
+        <Button
+          variant={`contained__${location}`}
+          name="Sign in"
+          onClick={() => navigate("/sign-in")}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
