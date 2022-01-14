fa36a8ddb25ab406f58bc36d852dbc6e3dfedc8e
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-14 13:09:00 +0300

    Validation complete

--- src/components/SignInPage/SignInPage.jsx
+++ src/components/SignInPage/SignInPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import SignInPageClasses from "./SignInPage.module.css";
 import Header from "../Header/Header";
@@ -7,7 +7,37 @@ import Input from "../UI/Input/Input";
 import Button from "../UI/Button/Button";
 
 const SignInPage = () => {
+  const [isDisableBtn, setIsDisableBtn] = useState(true);
+  const [formState, setFormState] = useState({
+    firstName: {
+      value: "",
+      isValid: false,
+    },
+    lastName: {
+      value: "",
+      isValid: false,
+    },
+    email: {
+      value: "",
+      isValid: false,
+    },
+    password: {
+      value: "",
+      isValid: false,
+    },
+  });
 
+  const validState = [];
+  useEffect(() => {
+    Object.keys(formState).map((i) => {
+      validState.push(formState[i].isValid);
+    });
+    Object.keys(validState).map((i) => {
+      console.log(validState.filter((state) => !state).length);
+      if (validState.filter((state) => !state).length) setIsDisableBtn(true);
+      else setIsDisableBtn(false);
+    });
+  }, [formState]);
   return (
     <>
       <Header />
@@ -16,18 +46,42 @@ const SignInPage = () => {
           Create your free account
         </h1>
         <form noValidate={true} className={SignInPageClasses[`signing__form`]}>
-          <Input text="First name" name="firstName" type="text" notValidText='Please enter a first name.' />
-          <Input text="Last name" name="lastName" type="text" notValidText='Please enter a last name.' />
+          <Input
+            text="First name"
+            name="firstName"
+            type="text"
+            notValidText="Please enter a first name."
+            formState={formState}
+            setFormState={setFormState}
+          />
+          <Input
+            text="Last name"
+            name="lastName"
+            type="text"
+            notValidText="Please enter a last name."
+            formState={formState}
+            setFormState={setFormState}
+          />
           <Input
             text="Email Address"
             name="email"
             type="email"
-            notValidText='Please enter your username or email address.'
+            notValidText="Please enter your username or email address."
+            formState={formState}
+            setFormState={setFormState}
           />
-          <Input text="Password" name="password" type="password" notValidText='Please enter a password.' />
+          <Input
+            text="Password"
+            name="password"
+            type="password"
+            notValidText="Please enter a password."
+            formState={formState}
+            setFormState={setFormState}
+          />
           <Button
             name="Create account"
             variant="contained__login"
+            isDisable={isDisableBtn}
           />
         </form>
       </div>
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 
+import InputClasses from "./Input.module.css";
 import { Patterns } from "../../../appConstants";
 
-import InputClasses from "./Input.module.css";
-
-const Input = ({ text, name, type, notValidText}) => {
-  const [isValid, setIsValid] = useState(true)
-
-  const handleChange = (e) => {
-    if (!Patterns[e.target.name].test(e.target.value) && e.target.value) setIsValid(false)
-    else setIsValid(true)
-  }
-
+const Input = ({ text, name, type, notValidText, formState, setFormState }) => {
+  const [isValid, setIsValid] = useState(true);
+  const newInputValue = (e) => {
+    setFormState({
+      ...formState,
+      [name]: {
+        value: e.target.value,
+        isValid: Patterns[name].test(e.target.value),
+      },
+    });
+    if (!Patterns[name].test(e.target.value) && e.target.value) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  };
   return (
     <label className={InputClasses[`input__block`]}>
       <p className={InputClasses[`input__name`]}>{text}</p>
@@ -19,11 +26,13 @@ const Input = ({ text, name, type, notValidText}) => {
         className={InputClasses[`input__${isValid}`]}
         type={type}
         name={name}
-        onChange={(e) => handleChange(e)}
+        onChange={(e) => newInputValue(e)}
       />
-      {!isValid &&
-      <p className={InputClasses[`input__not__valid__text`]}>{notValidText}</p>
-      }
+      {!isValid && (
+        <p className={InputClasses[`input__not__valid__text`]}>
+          {notValidText}
+        </p>
+      )}
     </label>
   );
 };
