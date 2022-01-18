8440ac4cb6eefb66274f90e91518508d4549d1c2
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-18 17:50:29 +0300

    create add article form

--- src/components/AddArticle/AddArticle.jsx
+++ src/components/AddArticle/AddArticle.jsx
@@ -1,39 +1,71 @@
 import React, { useState } from "react";
-import { Editor, EditorState } from "draft-js";
-import "draft-js/dist/Draft.css";
-
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 import AddArticleClasses from "./AddArticle.module.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
 const AddArticle = () => {
+  const [isDisableBth, setIsDisableBth] = useState(true);
+  const [inputValue, setInputValue] = useState("");
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const [convertedContent, setConvertedContent] = useState(null);
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
 
-  const editor = React.useRef(null);
-
-  function focusEditor() {
-    editor.current.focus();
-  }
-
-  React.useEffect(() => {
-    focusEditor();
-  }, []);
-
+  const clickSubmitBth = (e) => {};
   return (
     <>
       <Header />
-      <Editor
-        ref={editor}
-        editorState={editorState}
-        onChange={setEditorState}
-        placeholder="Write something!"
-      />
+      <main className={AddArticleClasses[`editor__section`]}>
+        <h1 className={AddArticleClasses.title}>Add Article</h1>
+        <Input
+          text=""
+          name="title"
+          placeholder="Enter a title"
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
+        <Input
+          text=""
+          name="subtitle"
+          placeholder="Enter the category name..."
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
+        <div className={AddArticleClasses.editor}>
+          <Editor
+            onEditorStateChange={handleEditorChange}
+            wrapperClassName="wrapper-class"
+            editorClassName="editor-class"
+            toolbarClassName="toolbar-class"
+            placeholder="Enter the text..."
+          />
+        </div>
+        <Button
+          name="Publish an article"
+          variant="contained__header"
+          onClick={(e) => {
+            clickSubmitBth(e);
+          }}
+          isDisable={isDisableBth}
+        />
+      </main>
       <Footer />
     </>
   );
 };
-
 export default AddArticle;
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import InputClasses from "./Input.module.css";
 import { Patterns } from "../../../appConstants";
@@ -11,6 +11,7 @@ const Input = ({
   formState,
   setFormState,
   inputValue,
+  placeholder,
   setInputValue,
 }) => {
   const [isValid, setIsValid] = useState(true);
@@ -29,18 +30,19 @@ const Input = ({
       } else {
         setIsValid(true);
       }
-    } else {
+    } else if (inputValue) {
       setInputValue({ ...inputValue, [name]: e.target.value });
     }
   };
 
   return (
     <label className={InputClasses[`input__block`]}>
-      <p className={InputClasses[`input__name`]}>{text}</p>
+      {text && <p className={InputClasses[`input__name`]}>{text}</p>}
       <input
         className={InputClasses[`input__${isValid}`]}
         type={type}
         name={name}
+        placeholder={placeholder}
         value={inputValue[name]}
         onChange={(e) => newInputValue(e)}
       />
